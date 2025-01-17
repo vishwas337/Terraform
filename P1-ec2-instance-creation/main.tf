86250dbd90808a0993982ecbf0e95c35da38b8f0
vishwas337 <vishwassunkari07@gmail.com>
2025-01-17 12:24:53 +0000

    new commits

---
+++ P1-ec2-instance-creation/main.tf
@@ -0,0 +1,8 @@
+provider "aws" {
+    region = "us-east-1"  # Set your desired AWS region
+}
+
+resource "aws_instance" "vishwas" {
+    ami           = "ami-0e2c8caa4b6378d8c"  # Specify an appropriate AMI ID
+    instance_type = "t2.micro"
+}
